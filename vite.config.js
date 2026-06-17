cbc57a82619d13bf6c49aea06eda8915d7e304c5
Eduard Guerrero <guerrero.a.eduard@gmail.com>
2026-06-17 15:47:04 +0100

    update: config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/world-cooking',
+  base: '/world-cooking/',
   test: {
     environment: 'jsdom',
     globals: true,
